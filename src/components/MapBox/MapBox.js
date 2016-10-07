5f441a80d7931c40e462a6f0cf9622b561fb96b7
ShanzayA <shanzayamjad@hotmail.com>
2016-10-07 18:30:41 +0200

    Loaded css
    - need to reference marker image

--- src/components/MapBox/MapBox.js
+++ src/components/MapBox/MapBox.js
@@ -10,8 +10,7 @@
 import React, { Component, PropTypes } from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './MapBox.scss'
-// import { render } from 'react-dom';
-// import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+import { render } from 'react-dom';
 
 class MapBox extends Component {
 
@@ -44,49 +43,35 @@ class MapBox extends Component {
 
     //===========
 
-    // var {Map, TileLayer, Marker, Popup} = require('react-leaflet');
-    //
-    // var position = [this.props.lat, this.props.long];
-    //
-    // const map = (
-    //   <Map center={position} zoom="13">
-    //     <TileLayer
-    //       attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-    //       url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-    //     />
-    //     <Marker position={position}>
-    //       <Popup>
-    //         <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-    //       </Popup>
-    //     </Marker>
-    //   </Map>
-    // );
-    //
-    // render(map, document.getElementById('map'));
+    var {Map, TileLayer, Marker, Popup} = require('react-leaflet');
+    //require('leaflet/dist/leaflet.css');
+
+    var position = [this.props.lat, this.props.long];
+
+    const map = (
+      <Map center={position} zoom="13">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
+    );
+
+    //map.css('./leaflet.css');
+    render(map, document.getElementById('map'));
 
   }
 
 
   render() {
 
-    var {Map, TileLayer, Marker, Popup} = require('react-leaflet');
-    require('leaflet/dist/leaflet.css');
-
-    var position = [this.props.lat, this.props.long];
-
     return (
-      <div className={s.container}>
-        <Map center={position} zoom="13">
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-          />
-          <Marker position={position}>
-            <Popup>
-              <span>A pretty CSS3 popup. <br/> Easily customizable.</span>
-            </Popup>
-          </Marker>
-        </Map>
+      <div id="map" className={s.container}>
       </div>
     );
   }
