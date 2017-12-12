4b8e3e7d91a73b2db7dbf5039cec2917d634e328
Dennis Diefenbach <dennis.diefenbach@univ-st-etienne.fr>
2017-12-12 13:30:02 +0100

    Adjusted links bar so that the links are clickable under trigger

--- src/components/LinksBar/LinksBar.js
+++ src/components/LinksBar/LinksBar.js
@@ -23,17 +23,57 @@ class LinksBar extends Component {
     super(props);
   }
 
+  wikipedia(e){
+    window.open(this.props.links.wikipedia);
+    e.stopPropagation();
+  }
+
+  dbpedia(e){
+    window.open(this.props.links.dbpedia);
+    e.stopPropagation();
+  }
+
+  wikidata(e){
+    window.open(this.props.links.wikidata);
+    e.stopPropagation();
+  }
+
+  musicbrainz(e){
+    window.open(this.props.links.wikidata);
+    e.stopPropagation();
+  }
+
+  dblp(e){
+    window.open(this.props.links.dblp);
+    e.stopPropagation();
+  }
+
+  biennale(e){
+    window.open(this.props.links.biennale);
+    e.stopPropagation();
+  }
+
+  freebase(e){
+    window.open(this.props.links.freebase);
+    e.stopPropagation();
+  }
+
+  doi(e){
+    window.open(this.props.links.dopi);
+    e.stopPropagation();
+  }
+
   render() {
     return (
       <div className={s.container}>
-        {this.props.links.hasOwnProperty("wikipedia") == true ? <a target="_blank" href={this.props.links.wikipedia}><img src={require('./images/wikipedia-logo.png')} height="30" alt="wikipedia" className={s.imglink}/></a> : null}
-        {this.props.links.hasOwnProperty("dbpedia") == true ? <a target="_blank" href={this.props.links.dbpedia}><img src={require('../../actions/knowledge_base/implemented/images/dbpedia_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
-        {this.props.links.hasOwnProperty("wikidata") == true ? <a target="_blank" href={this.props.links.wikidata}><img src={require('../../actions/knowledge_base/implemented/images/wikidata_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
-        {this.props.links.hasOwnProperty("musicbrainz") == true ? <a target="_blank" href={this.props.links.musicbrainz}><img src={require('../../actions/knowledge_base/implemented/images/musicbrainz_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
-        {this.props.links.hasOwnProperty("dblp") == true ? <a target="_blank" href={this.props.links.dblp}><img src={require('../../actions/knowledge_base/implemented/images/dblp_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
-        {this.props.links.hasOwnProperty("biennale") == true ? <a target="_blank" href={this.props.links.biennale}><img src={require('../../actions/knowledge_base/implemented/images/biennale_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
-        {this.props.links.hasOwnProperty("freebase") == true ? <a target="_blank" href={this.props.links.freebase}><img src={require('../../actions/knowledge_base/implemented/images/freebase_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
-        {this.props.links.hasOwnProperty("doi") == true ? <a target="_blank" href={this.props.links.doi}><img src={require('./images/doi-logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
+        {this.props.links.hasOwnProperty("wikipedia") == true ? <a href="" onClick={this.wikipedia.bind(this)}><img src={require('./images/wikipedia-logo.png')} height="30" alt="wikipedia" className={s.imglink}/></a> : null}
+        {this.props.links.hasOwnProperty("dbpedia") == true ? <a href="" onClick={this.dbpedia.bind(this)}><img src={require('../../actions/knowledge_base/implemented/images/dbpedia_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
+        {this.props.links.hasOwnProperty("wikidata") == true ? <a href="" onClick={this.wikidata.bind(this)}><img  src={require('../../actions/knowledge_base/implemented/images/wikidata_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
+        {this.props.links.hasOwnProperty("musicbrainz") == true ? <a href="" onClick={this.musicbrainz.bind(this)}><img  src={require('../../actions/knowledge_base/implemented/images/musicbrainz_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
+        {this.props.links.hasOwnProperty("dblp") == true ? <a href="" onClick={this.dblp.bind(this)}><img  src={require('../../actions/knowledge_base/implemented/images/dblp_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
+        {this.props.links.hasOwnProperty("biennale") == true ? <a href="" onClick={this.biennale.bind(this)}><img src={require('../../actions/knowledge_base/implemented/images/biennale_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
+        {this.props.links.hasOwnProperty("freebase") == true ? <a href="" onClick={this.freebase.bind(this)}><img onClick={this.freebase.bind(this)} src={require('../../actions/knowledge_base/implemented/images/freebase_logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
+        {this.props.links.hasOwnProperty("doi") == true ? <a href="" onClick={this.doi.bind(this)}><img  src={require('./images/doi-logo.png')} height="30" alt="resource" className={s.imglink}/></a> : null}
       </div>
     );
   }
